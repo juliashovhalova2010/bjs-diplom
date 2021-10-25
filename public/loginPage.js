d99c832857fcbb1754e1cafdb7067b85ff689ffe
juliashovhalova2010 <83767399+juliashovhalova2010@users.noreply.github.com>
2021-10-25 10:52:55 +0300

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,19 +1,22 @@
 'use strict';
 const userForm = new UserForm();
-userForm.loginFormCallback = data => ApiConnector.login(data, response => {
-    if (!response.success) {
-        return userForm.setLoginErrorMessage(response.error);
-    }
-    userForm.loginFormAction(response);
-    location.reload();
-    return;
-});
 
-userForm.registerFormCallback = newData => ApiConnector.register(newData, response => {
-    if (!response.success) {
-        return userForm.setRegisterErrorMessage(response.error);
+userForm.loginFormCallback = (data) => 
+{ ApiConnector.login(data, (response) => {
+    if (response.success) {
+        location.reload();
+    } else {
+        userForm.setLoginErrorMessage(`Произошла ошибка: ${response.error}`);
+        }
+    })
+};
+
+userForm.registerFormCallback = (data) => 
+{ ApiConnector.register(data, (response) => {
+    if (response.success) {
+        location.reload();
+    } else {
+    userForm.setRegisterErrorMessage(`${response.error}`);
     }
-    userForm.registerFormAction(response);
-    location.reload();
-    return;
-});
+   })
+};
